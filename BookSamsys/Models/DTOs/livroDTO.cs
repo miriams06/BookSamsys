624248933f5f0825cdc200b8a20aa8cec70b2155
Miriam Silva <miriambiasilva@gmail.com>
2024-02-09 16:32:24 +0000

    criei um dto para o livro

---
+++ BookSamsys/Models/DTOs/livroDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookSamsys.Models.DTOs
+{
+    public class livroDTO
+    {
+            public string ISBN { get; set; }
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public decimal Preco { get; set; }
+    }
+}
