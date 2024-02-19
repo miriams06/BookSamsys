a79d0e05c31c96d12cc64a16700d3ce2422ce348
Miriam Silva <miriambiasilva@gmail.com>
2024-02-19 18:50:30 +0000

    criei um messaging helper

---
+++ BookSamsys/MessagingHelper/MessagingHelper.cs
@@ -0,0 +1,13 @@
+﻿using BookSamsys.Controllers;
+using BookSamsys.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using BookSamsys.Models;
+using BookSamsys.Repository;
+using BookSamsys.Services;
+
+public class MessagingHelper<T>
+{
+    public bool Sucesso { get; set; }
+    public string Mensagem { get; set; }
+    public T Obj { get; set; }
+}
