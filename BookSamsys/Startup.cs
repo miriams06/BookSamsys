73c736745186d922b89a53e60c847f4c9f877220
Miriam Silva <miriambiasilva@gmail.com>
2024-04-23 15:33:44 +0100

    fiz alterações para ter permissão para aceder à api

--- BookSamsys/Startup.cs
+++ BookSamsys/Startup.cs
@@ -2,8 +2,14 @@
 using BookSamsys.Repository;
 using BookSamsys.Services;
 using BookSamsys.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using System.Text.Json.Serialization;
-using AutoMapper;
+
 namespace BookSamsys
 {
     public class Startup
@@ -18,6 +24,13 @@ namespace BookSamsys
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
+
+            services.AddDbContext<AppDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
             services.AddScoped<ILivroService, LivroService>();
             services.AddScoped<IAutorService, AutorService>();
@@ -26,36 +39,51 @@ namespace BookSamsys
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("_myAllowSpecificOrigins",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+                    });
+            });
 
-            services.AddDbContext<AppDBContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
-
-
-            services.AddCors();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookSamsys");
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseCors("_myAllowSpecificOrigins");
+
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
-
     }
 }
